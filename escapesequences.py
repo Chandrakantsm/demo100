d187c9598a6270bda59c638e269d3694c642d7be
Chandrakantsm <124190732+Chandrakantsm@users.noreply.github.com>
2023-02-21 18:24:48 +0530

    Add files via upload

--- escapesequences.py
+++ escapesequences.py
@@ -1,3 +1,77 @@
-print("\\") #back slash
-print("\\ hello\\")
+# \\ Back slash
+# print("\\")
+# print("\\hello\\")
+# print("\hello\\")  # here h shoulnt be printed, but  giving correct result
+
+
+# # \' single Quote
+# print("'hello'")
+# print("\'hello\'")  # why need \ when getting the same output as above
+# print("'sainik school amaravatinagar'") 
+# print("\'sainik school amaravatinagar\'")#why need \ when getting the same output as above
+# print(" 'praveen jigjinni'")
+# print("\'praveen jigjinni\'") #why need \ when getting the same output as above
+
+
+# # \" Double quote
+# print("\"sainik school\"") # 2 double quotes together will lead to syntax error
+# print("\"python programming\"")
+
+# # \a ASCII bell
+# print("\a")     # sound not comig in headphone , mobiles, computers
+# print("\a\a\a\acomputer programming is fun")  
+# print("\a computer \a programming \a is \a fun")
+
+## \b ASCII Backspace
+#print("\bpython\b is OOP's language") #it will eat previous letter, but why not "\
+
+# # \f ASCII formfeed 
+# print(" \f python programming")
+
+# # \n New line
+# print("welcome \nto \npython \nprogramming")
+
+# # \r carriage return
+# print("\rpython programming")
+
+# # \t Horizontal tab
+# print("\twel\tcome\tto\tpython")
+
+# #\v vertical tab
+# print("\v\vpython")  # vertical rtab not coming
+# print("hello \v python " )
+
+# # 16 bit hex value
+# print("\x41")
+# print("\x42")
+
+# # \ooo octal value
+# print("\110")
+# print("\000")
+# print("\112")
+# print("\120")
+
+
+
+
+
+
+
+# \a ASCII bell
+
+# \b ASCII back space
+
+# \f ASCII form feed 
+
+# \n New line
+
+# \r Carriage return
+
+#\t  Horizontal Tab
+
+# \v vertical tab
+
+# \x 16 bit Hex value
+
+#\OOO Octal value
 

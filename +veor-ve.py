250c35d6e0057b2afba6a925c5276b7509f021a5
Chandrakantsm <124190732+Chandrakantsm@users.noreply.github.com>
2023-02-21 18:25:37 +0530

    Add files via upload

---
+++ +veor-ve.py
@@ -0,0 +1,12 @@
+#prg to find, weather a number is positive or negative or zero
+
+num=1
+if num>0:
+    print("The number is positive")
+    elseif num<0:
+    print("the number is negative")
+else
+    print("The number is zero")
+
+# print("\v hello  \vhipython\v  world\v")
+#print("\110")

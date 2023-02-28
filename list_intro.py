be60006b6137b835804f191dd555bf761323358d
Chandrakantsm <124190732+Chandrakantsm@users.noreply.github.com>
2023-02-28 17:20:53 +0530

    Add files via upload

---
+++ list_intro.py
@@ -0,0 +1,9 @@
+# creating a list
+newlist=['python',25,2022]
+#print(newlist)
+
+print("positive and negative indexing of the first element\npositive index",newlist[0],"\nNegative index",newlist[-3] )
+
+print("positive and negative indexing of the second element\npositive index",newlist[1],"\nNegative index",newlist[-2] )
+
+print("positive and negative indexing of the third element\npositive index",newlist[2],"\nNegative index",newlist[-1] )

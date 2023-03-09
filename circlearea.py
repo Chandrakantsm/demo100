f0afcb9443f5cbb97ddc563ab423a8813bee58bb
Chandrakantsm <chandrakantsm@gmail.com>
2023-03-09 19:41:12 +0530

    one more change

--- circlearea.py
+++ circlearea.py
@@ -1,4 +1,5 @@
 ##prg to find the area of a circle
 r=float(input("Enter the radius of the circle"))
 area=3.142*r*r
-print(" Area of the circle is ",area)       
+print(" Area of the circle is ",area)    
+print("prg running successfully")   

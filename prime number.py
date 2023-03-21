f9a6501dffe1572dd50761b29facbbd2741a4de0
Chandrakantsm <chandrakantsm@gmail.com>
2023-03-21 18:13:32 +0530

    first edit

--- prime number.py
+++ prime number.py
@@ -1,14 +1,15 @@
 n=int(input("Enter a number"))
 Flag=False
 
-if n%2==0:
-    print(n,"is  not a prime number ")
-else:
-    for i in range(3,n):
+for j in range(2,n+1):
+    # if n%2==0:
+    #  print(j,"is  not a prime number ")
+    # else:
+     for i in range(3,n+1):
         if n%i==0:
             Flag=True   
             break 
-    if Flag:
-        print(n,"is not a prime number") 
-    else:
-        print(n,"is  a prime number")       
+     if Flag:
+      print(j,"is not a prime number") 
+     else:
+      print(j,"is  a prime number")       

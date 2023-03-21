65f2105e71f2d041ccc451ee2d942a70c6a7e0ec
Chandrakantsm <chandrakantsm@gmail.com>
2023-03-21 19:33:01 +0530

    first edit

--- twofunctions.py
+++ twofunctions.py
@@ -1,19 +1,24 @@
 # prg to create marks card
 
-def getmarks(sub1,sub2): 
-    sub1=int(input("maths marks pls"))
-    sub2=int(input("stats marks pls"))
-    
-def avg(): 
+# def getmarks(sub1,sub2): 
+#     sub1=int(input("maths marks pls"))
+#     sub2=int(input("stats marks pls"))
+#     return 
+
+sub1=50
+sub2=60
+
+
+def avg(sub1,sub2): 
     result = (sub1+sub2)/2
     return result
     
 def exam_result(): 
-    if ((avg)>35):   
+    if (avg()>35):
         print("student passes")
     else:   
         print("student fails")
         
-print(exam_result())
+exam_result()
 
 

59a863576622010e23c5b7de2cdf013fbfd6e7bb
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-02 15:12:15 +0530

    2 more functions added

--- functions2.py
+++ functions2.py
@@ -1,22 +1,53 @@
-#program find wheather sum is odd or even
+# #program find wheather sum is odd or even
 
-def getdata():
-    a=int(input("Enter a number"))
-    b=int(input("Enter a number"))
-    print("The two numbers are",a,b)
-    addition(a,b)
+# def getdata():
+#     a=int(input("Enter a number"))
+#     b=int(input("Enter a number"))
+#     print("The two numbers are",a,b)
+#     addition(a,b)
     
-def addition(a,b):
-    sum=a+b
-    print("The sum of two number is", sum)
-    oddeven(sum)
+# def addition(a,b):
+#     sum=a+b
+#     print("The sum of two number is", sum)
+#     oddeven(sum)
     
-def oddeven(sum):
-    if sum%2==0:
-        print("The sum ",sum," is even")
-    else:
-        print("The sum ",sum," is odd")  
-    choice=input("Do you want to continue?:y or n")
-    while choice == "y":
-        return getdata()
-getdata()          
+# def oddeven(sum):
+#     if sum%2==0:
+#         print("The sum ",sum," is even")
+#     else:
+#         print("The sum ",sum," is odd")  
+#     choice=input("Do you want to continue?:y or n")
+#     while choice == "y":
+#         return getdata()
+# getdata()    
+
+# def main():
+#         even(56)
+#         odd(5)
+
+# def even(n):
+#     if n % 2 == 0 :
+#         print(n,"is the even number")
+
+# def odd(n):
+#     if n % 2 != 0 :
+#         print(n,"is the odd number")
+        
+# main()  
+
+def main():
+    p=1000  
+    t=2
+    r=8
+    interest(p,t,r)
+    
+def interest(p,t,r) :
+    si=p*t*r/100  
+    display(si)
+    
+def display(si):
+    print("simple interest is Rs", si )
+    
+main()    
+        
+    
--- prime number.py
+++ prime number.py
@@ -15,18 +15,27 @@
 #       print(j,"is  a prime number")       
       
       
+      
 n=int(input("Enter a number"))
-Flag=False
+flag=False
 
-# for j in range(2,n+1):
-    # if n%2==0:
-    #  print(j,"is  not a prime number ")
-    # else:
-for i in range(3,n+1):
-        if n%i==0:
-            Flag=True   
-            break 
-if Flag:
-      print(i,"is not a prime number") 
-else:
-      print(i,"is  a prime number")  
+for j in range(2,n+1):
+    if j%2 == 0:
+     print(j,"is  not a prime number ")
+    else:
+         for i in range(3,n+1):
+             if j%i==0:
+                 print(j,"is not a prime number")
+             else:
+                 print(j,"is a prime number")    
+             
+
+        
+# for i in range(3,n+1):
+#         if n%i==0:
+#             Flag=True   
+#             break 
+# if Flag:
+#       print(i,"is not a prime number") 
+# else:
+#       print(i,"is  a prime number")  

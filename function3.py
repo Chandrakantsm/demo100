d990034a534b9f6dc4c81bf74f974302853d6290
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-02 13:28:08 +0530

    prg itself not running

--- function3.py
+++ function3.py
@@ -1,9 +1,17 @@
 def main():
     a=10
+    result=concat_function(a)
+    my_string="the entered value"+str(a)+"is even" "with remainder: " +str(result)
+    display_result(my_string,result)
     
+        
 def concat_function(x):
-    
-    
-def display_result(): 
+    return x % 2
+          
+def display_result(my_string,result): 
+    if(result % 2 == 0):
+        print("Entered value is even\n",my_string)
+    else:
+        print("Entered value is odd\n",my_string)    
     
 main()            

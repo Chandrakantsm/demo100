1c5918f755dc206b48648162f472104d4b397e5d
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-05 14:54:56 +0530

    hi

--- function3.py
+++ function3.py
@@ -1,17 +1,23 @@
-def main():
-    a=10
-    result=concat_function(a)
-    my_string="the entered value " + str(a) + "is even" "with remainder: " + str(result)
-    display_result(my_string,result)
+
+
+# def main():
+#     a=111
+#     result=concat_function(a)
+#     my_string="the entered value " + str(a) + "is even"+ "with remainder: " + str(result)
+#     display_result(my_string,result)
     
         
 def concat_function(x):
     return x % 2
           
-def display_result(my_string,result): 
-    if(result % 2 == 0):
-        print("Entered value is even\n",my_string)
-    else:
-        print("Entered value is odd\n",my_string)    
+# def display_result(my_string,result): 
+#     if(result % 2 == 0):
+#         print("Entered value is even\n",my_string)
+#     else:
+#         print("Entered value is odd\n",my_string)    
     
-main()            
+val=concat_function(111)
+print(val)
+
+
+    

bd1ebd019f20bd050fd0b78d5b7e8045e71aec65
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-08 16:23:41 +0530

    comit it

--- twofunctions.py
+++ twofunctions.py
@@ -1,13 +1,17 @@
 # prg to create marks card
 
+#  This function will get the marks in sub1 and sub2
 def getmarks(sub1,sub2): 
-    sub1=int(input("maths marks pls"))
-    sub2=int(input("stats marks pls"))
+    sub1 = int(input("maths marks pls"))
+    sub2 = int(input("stats marks pls"))
+ 
+#This function will calculte th percentage    
     
 def calculate(): 
-    total = sub1+sub2   
+    total = sub1 + sub2   
     avg = total/2
-    
+ 
+# This function will classify the result    
 def exam_result(): 
     if (avg>35):   
         result = pass

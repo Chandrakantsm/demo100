d5e1a329756639c9d16b28642aa4aa7ee3a00fe6
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-12 15:28:58 +0530

    undefined variables p t r

--- trainglearealive.py
+++ trainglearealive.py
@@ -1,17 +1,16 @@
 
-
+# prg to find the area of triangle
 
 def getdata():
     b=float(input("enter base value:"))
     h=float(input("enter height value:"))
+    ans=tri_area(b,h)
+    return ans
     
 def tri_area(b,h):
     area= b*h/2   
     return area
-   
-ans=tri_area()
-print(ans)
 
 
-getdata()
-
+a=getdata()
+print(a)

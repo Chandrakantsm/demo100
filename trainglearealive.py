1c9a828040b3df23d66e610dea2f1c2e73ffad26
Chandrakantsm <chandrakantsm@gmail.com>
2023-04-12 14:44:56 +0530

    done

---
+++ trainglearealive.py
@@ -0,0 +1,17 @@
+
+
+
+def getdata():
+    b=float(input("enter base value:"))
+    h=float(input("enter height value:"))
+    
+def tri_area(b,h):
+    area= b*h/2   
+    return area
+   
+ans=tri_area()
+print(ans)
+
+
+getdata()
+

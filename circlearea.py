ec0657a0320d4c08f1a63b9950ce866c9faa6c6c
Chandrakantsm <124190732+Chandrakantsm@users.noreply.github.com>
2023-04-15 17:36:48 +0530

    Update circlearea.py

--- circlearea.py
+++ circlearea.py
@@ -2,4 +2,51 @@
 r=float(input("Enter the radius of the circle"))
 area=3.142*r*r
 print(" Area of the circle is ",area)    
-print("prg running successfully")   
+print("prg running successfully")   15 prime numbers from 1 to 100
+# flag=False
+# for i in range(3,100):
+#     if i%2==0:
+#         print(i,"is not a prime number.")
+#     else:
+#         for j in range(3,i):
+#             if i%j==0:
+#                 flag=True
+#                 break
+#         if flag:
+#             print(i,"is not a prime number.")
+#         else:
+#             print(i,"is a prime number.")
+
+# 16 factorial number
+# n=10
+# fact=1
+# while n>1:
+#     fact=fact*n
+#     n=n-1
+#     print(fact)
+# print("***")    
+# print(fact)   
+                    
+#  17 Fibonacci sequence upto 1000
+# n1=0
+# n2=1
+# fib=0
+# while fib<1000:
+#     fib=n1+n2
+#     n1,n2=n2,fib
+#     print(fib)
+
+# 17 numbers divisible by another number
+
+# 18 convert decimal to binary  octal hexadecimal
+# n=15
+# print("decimal",n," in binary system is", bin(n),".")
+# print("decimal",n," in octal system is", oct(n),".")
+# print("decimal",n," in binary system is", hex(n),".")
+
+#  19 find value of a character
+# c=")"
+# print("The ASCHII value of    ",c,"     is  ",ord(c),".")
+
+# 20 Factors of a number
+# n=10000254

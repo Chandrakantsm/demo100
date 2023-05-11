367e8538433946bdd098f163182ba6fb134f62d2
Chandrakantsm <chandrakantsm@gmail.com>
2023-05-11 19:18:48 +0530

    test_ odd_list

---
+++ test_list_odd.py
@@ -0,0 +1,7 @@
+# prg to find a number is odd numbers in the list 
+list=[10,9,8,7,6,5,4,3,2,1]
+
+# for loop iterates thru all the elements of the list 
+for i in list:
+    if list[i]%2==1:
+        print(" the number ", list[i], "is odd")

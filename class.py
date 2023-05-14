efc4a1fde27c175b6a53b564d3191c83eac6a3ce
Chandrakantsm <chandrakantsm@gmail.com>
2023-05-14 12:53:38 +0530

    class creation

---
+++ class.py
@@ -0,0 +1,18 @@
+class truck:
+    # properties or attributes
+    seat=2
+    tyre=6
+    fuel="D"
+    carrier="goods"
+    fuelstate="True"
+    
+    # methods
+    def drive(self):
+        if self.fuelstate==True:
+            print("REady to drive. Fuel available.")
+        else:
+            print("no fuel available") 
+    def __init__(self,seat,tyre):  
+        self.         
+            
+    

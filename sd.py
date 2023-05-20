9b7830359df3206d36c8369464540e2d69d41350
Chandrakantsm <chandrakantsm@gmail.com>
2023-05-20 15:08:19 +0530

    hi

---
+++ sd.py
@@ -0,0 +1,11 @@
+# li=[2,2,3,4,6,7]
+
+# avg=sum(li)/len(li)
+
+import math.sqrt()
+
+a=sqrt(1.9)
+print(a)
+
+
+
--- stats.py
+++ stats.py
@@ -13,13 +13,13 @@ with open("C:/Users/User4/Desktop/stats.csv","r") as marks:
         if  i[1]!="marks1":
             marks1.append(int(i[1]))
             
-    for j in file_handle:        
-        if   j[2]!="marks2":
-            marks2.append(int(j[2]))   
+    # for j in file_handle:        
+    #     if   j[2]!="marks2":
+    #         marks2.append(int(j[2]))   
             
-    for i in file_handle:
-            if  i[1]!="marks1":
-            marks1.append(int(i[1]))        
+    # for i in file_handle:
+    #         if  i[1]!="marks1":
+    #         marks1.append(int(i[1]))        
             
 print("marks1 =",marks1)        
 # mean=st.mean(marks1) 
@@ -29,6 +29,8 @@ print("marks1 =",marks1)
 print("Mean=",round(st.mean(marks1),2),". median=",round(st.median(marks1),2) ,". mode=", round(st.mode(marks1),2))
 
 print("marks2= ",marks2)
+print("marks2= ",st.pstdev(marks1))
+
          
     
     
